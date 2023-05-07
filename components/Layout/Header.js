3795a4e752d579ee301e434e1c85cf7167aa21b6
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2023-05-07 22:56:28 +0530

    changes

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -6,17 +6,25 @@ import shockedEmoji from '../../assests/navbar/shockedEmoji.png'
 
 const Navbar = () => {
 return(
-     <nav className='mx-auto h-max pt-5'>
+     <nav className='mx-auto h-max pt-10'>
 
-       <div className='mobile:w-[200px] flex justify-center gap-4 mx-auto border-2 rounded-full py-2 w-[240px]'>
+       <div className='mobile:w-[200px] flex justify-center gap-7 mx-auto rounded-full py-2 w-[330px] border-2 border-[#9747FF] bg-[#19222F]'>
 
+          <div className='w-14 h-14 bg-[rgba(123,75,255,.31)] flex justify-center items-center rounded-full border-[1px] border-[#7b4bff]'>
           <img className='mobile:w-[30px] w-[40px] cursor-pointer' src={crazyEmoji} alt='emoji' /> 
+          </div>
 
+          <div className='w-14 h-14 bg-[rgba(123,75,255,.31)] flex justify-center items-center rounded-full border-[1px] border-[#7b4bff]'>
           <img className='mobile:w-[30px] w-[40px] cursor-pointer' src={coolEmoji} alt='emoji' /> 
+          </div>
 
+          <div className='w-14 h-14 bg-[rgba(123,75,255,.31)] flex justify-center items-center rounded-full border-[1px] border-[#7b4bff]'>
           <img className='mobile:w-[30px] w-[40px] cursor-pointer' src={loveEmoji} alt='emoji' /> 
+          </div>
 
+          <div className='w-14 h-14 bg-[rgba(123,75,255,.31)] flex justify-center items-center rounded-full border-[1px] border-[#7b4bff]'>
           <img className='mobile:w-[30px] w-[40px] cursor-pointer' src={shockedEmoji} alt='emoji' /> 
+          </div>
 
        </div>
 
