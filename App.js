80470f9adb9af96a2f62f0bfba0bc5dbab9e7f1a
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2023-05-15 15:47:10 +0530

    updates changes

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import Header from "./components/Layout/Header";
 import React from "react";
 import ReactDOM  from "react-dom/client";
+import { SolidButtons } from "./components/Layout/Header";
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 const AppLayout = () => {
     return(
+        <>
         <Header />
+        </>
     )
 }
 root.render(<AppLayout />)
--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -3,39 +3,41 @@ import coolEmoji from '../../assests/navbar/coolEmoji.png'
 import loveEmoji from '../../assests/navbar/loveEmoji.png' 
 import shockedEmoji from '../../assests/navbar/shockedEmoji.png' 
 
-
-const Navbar = () => {
-return(
-     <nav className='mx-auto h-max pt-10'>
-
-       <div className='mobile:w-[200px] flex justify-center gap-7 mx-auto rounded-full py-2 w-[330px] border-2 border-[#9747FF] bg-[#19222F]'>
-
-          <div className='w-14 h-14 bg-[rgba(123,75,255,.31)] flex justify-center items-center rounded-full border-[1px] border-[#7b4bff]'>
-          <img className='mobile:w-[30px] w-[40px] cursor-pointer' src={crazyEmoji} alt='emoji' /> 
-          </div>
-
-          <div className='w-14 h-14 bg-[rgba(123,75,255,.31)] flex justify-center items-center rounded-full border-[1px] border-[#7b4bff]'>
-          <img className='mobile:w-[30px] w-[40px] cursor-pointer' src={coolEmoji} alt='emoji' /> 
-          </div>
-
-          <div className='w-14 h-14 bg-[rgba(123,75,255,.31)] flex justify-center items-center rounded-full border-[1px] border-[#7b4bff]'>
-          <img className='mobile:w-[30px] w-[40px] cursor-pointer' src={loveEmoji} alt='emoji' /> 
-          </div>
-
-          <div className='w-14 h-14 bg-[rgba(123,75,255,.31)] flex justify-center items-center rounded-full border-[1px] border-[#7b4bff]'>
-          <img className='mobile:w-[30px] w-[40px] cursor-pointer' src={shockedEmoji} alt='emoji' /> 
-          </div>
-
-       </div>
-
-     </nav>
-)
-}
 const Header = () => {
-    return(
-        <>
-        <Navbar />
-        </>
-    )
+  return(
+    <div className='flex w-full justify-center'>
+      <div className="flex w-[330px] bg-[#19222F] border border-r-[#7b4bff] rounded-full p-1.5 mt-3 justify-center items-center gap-4 space-y-2 md:flex-row md:space-x-2 md:space-y-0">
+        
+      <div className='w-16 mt-2 h-[60px] rounded-full cursor-pointer'>
+      <img src={crazyEmoji}
+        type="button"
+        className="w-full h-full rounded-full bg-[#382E70] px-3 py-2 text-sm font-semibold border border-[#7b4bff] text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+      />
+      </div>
+
+      <div className='w-16 h-[60px] rounded-full cursor-pointer'>
+      <img src={coolEmoji}
+        type="button"
+        className="w-full h-full rounded-full hover:bg-[#382E70] px-3 py-2 text-sm font-semibold border border-transparent hover:border-[#7b4bff] text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+      />
+      </div>
+      
+      <div className='w-16 h-[60px] rounded-full cursor-pointer'>
+      <img src={loveEmoji}
+        type="button"
+        className="w-full h-full rounded-full hover:bg-[#382E70] px-3 py-2 text-sm font-semibold border border-transparent hover:border-[#7b4bff] text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+      />
+      </div>
+
+      <div className='w-16 h-[60px] rounded-full cursor-pointer'>
+      <img src={shockedEmoji}
+        type="button"
+        className="w-full h-full rounded-full hover:bg-[#382E70] px-3 py-2 text-sm font-semibold border border-transparent hover:border-[#7b4bff] text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+      />
+      </div>
+    </div>
+    </div>
+  )
 }
+
 export default Header
